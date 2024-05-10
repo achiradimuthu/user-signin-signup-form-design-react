bc7e61dd37484a37bf0b4a2be335dcd41d950fae
Achira Dimuthu <bvachiradimuthu@gmail.com>
2024-05-10 15:50:50 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import SigninSignup from "./components/SigninSignup/SigninSignup";
+
+function App() {
+  return (
+    <>
+      <SigninSignup />
+    </>
+  );
+}
+
+export default App;
